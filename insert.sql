578b506e570999828064709ce163a61b69979c61
randrianasolo-faniry-niaina <randrianasolofaniry6@gmail.com>
2026-02-09 16:02:09 +0300

    modif script BdD

--- insert.sql
+++ insert.sql
@@ -1,3 +1,4 @@
+-- Active: 1741104595338@@127.0.0.1@3306@takalo
 CREATE DATABASE takalo
     DEFAULT CHARACTER SET = 'utf8mb4';
 
@@ -12,20 +13,16 @@ CREATE TABLE `objet`(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `lib` TEXT NOT NULL,
     `descritption` TEXT NOT NULL,
-    `id_proprio` INT NOT NULL
+    `id_proprio` INT UNSIGNED NOT NULL,
+    CONSTRAINT `objet_id_proprio_foreign` FOREIGN KEY(`id_proprio`) REFERENCES `user`(`id`)
 );
 CREATE TABLE `echange`(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `id_objet` INT NOT NULL,
-    `id_actuel` INT NOT NULL,
-    `id_new` INT NOT NULL,
-    `date_echange` DATE NOT NULL
+    `id_objet` INT UNSIGNED NOT NULL,
+    `id_actuel` INT UNSIGNED NOT NULL,
+    `id_new` INT UNSIGNED NOT NULL,
+    `date_echange` DATE NOT NULL,
+    CONSTRAINT `echange_id_objet_foreign` FOREIGN KEY(`id_objet`) REFERENCES `objet`(`id`),
+    CONSTRAINT `echange_id_actuel_foreign` FOREIGN KEY(`id_actuel`) REFERENCES `user`(`id`),
+    CONSTRAINT `echange_id_new_foreign` FOREIGN KEY(`id_new`) REFERENCES `user`(`id`)
 );
-ALTER TABLE
-    `echange` ADD CONSTRAINT `echange_id_objet_foreign` FOREIGN KEY(`id_objet`) REFERENCES `objet`(`id`);
-ALTER TABLE
-    `objet` ADD CONSTRAINT `objet_id_proprio_foreign` FOREIGN KEY(`id_proprio`) REFERENCES `user`(`id`);
-ALTER TABLE
-    `echange` ADD CONSTRAINT `echange_id_new_foreign` FOREIGN KEY(`id_new`) REFERENCES `user`(`id`);
-ALTER TABLE
-    `echange` ADD CONSTRAINT `echange_id_actuel_foreign` FOREIGN KEY(`id_actuel`) REFERENCES `user`(`id`);

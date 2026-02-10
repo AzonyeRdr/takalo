ddc649a9cd208e8bbf5c9d8a4a42926c4e026305
Bryan <ramboliarisoabryan1@gmail.com>
2026-02-10 09:39:29 +0300

    proposition init tables et runcate

---
+++ data/20260210-01-truncate.sql
@@ -0,0 +1,14 @@
+-- Script to truncate all tables in the takalo database
+-- Run this script to empty all data from the tables while keeping the structure
+
+TRUNCATE TABLE historique_proprietaire_objet;
+TRUNCATE TABLE echange_objets;
+TRUNCATE TABLE echanges;
+TRUNCATE TABLE statuts_echange;
+TRUNCATE TABLE photos_objet;
+TRUNCATE TABLE objets;
+TRUNCATE TABLE statuts_objet;
+TRUNCATE TABLE etats_objet;
+TRUNCATE TABLE categories;
+TRUNCATE TABLE utilisateurs;
+TRUNCATE TABLE roles;
